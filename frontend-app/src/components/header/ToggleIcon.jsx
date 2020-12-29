98ec82385ea5672e46947a98a85253da5597ab4f
andres <andres@clicknaranja.com>
2020-12-29 16:34:40 +0100

    Toggle search icon

--- frontend-app/src/components/header/ToggleIcon.jsx
+++ frontend-app/src/components/header/ToggleIcon.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const ToggleIcon = ({isOpen=false, OpenIcon, CloseIcon}) => {
-    return (
-        <button>
-            {isOpen && OpenIcon && <span className="toogle-icon__open"><OpenIcon /></span>}
-            {!isOpen && CloseIcon && <span className="toogle-icon__close"><OpenIcon /></span>}
-        </button>
-    );
+const ToggleIcon = ({ isOpen = false, OpenIcon, CloseIcon, onClick }) => {
+  return (
+    <button
+      className="toggle-icon"
+      aria-label={`${isOpen ? "open" : "closed"}`}
+      onClick={onClick}
+    >
+      {isOpen && OpenIcon && (
+        <span className="toogle-icon__open">
+          <OpenIcon />
+        </span>
+      )}
+      {!isOpen && CloseIcon && (
+        <span className="toogle-icon__close">
+          <OpenIcon />
+        </span>
+      )}
+    </button>
+  );
 };
 
 export default ToggleIcon;
--- frontend-app/src/components/header/ToggleIcon.test.js
+++ frontend-app/src/components/header/ToggleIcon.test.js
@@ -1,25 +1,35 @@
-import { render, fireEvent } from '@testing-library/react'; 
-import ToggleIcon from './ToggleIcon'
-import { ReactComponent as OpenIcon } from "../../assets/images/search.svg"
-import { ReactComponent as CloseIcon } from "../../assets/images/close.svg"
+import { render } from "@testing-library/react";
+import ToggleIcon from "./ToggleIcon";
+import { ReactComponent as OpenIcon } from "../../assets/images/search.svg";
+import { ReactComponent as CloseIcon } from "../../assets/images/close.svg";
 
-describe('ToogleIcon is a button and ...', () => {
-    
-    it('Has a open icon if closed', () => {
-        const {container} = render(<ToggleIcon isOpen={true} OpenIcon={OpenIcon} />)
-        const openIconContainer = container.firstChild.querySelector('.toogle-icon__open')
-        expect(openIconContainer).toBeTruthy()
-    })
-    it('Has a close icon if open', () => {
-        const {container} = render(<ToggleIcon isOpen={false} OpenIcon={OpenIcon} CloseIcon={CloseIcon} />)
-        const openIconContainer = container.firstChild.querySelector('.toogle-icon__close')
-        expect(openIconContainer).toBeTruthy()
-    })
+describe("ToogleIcon is a button and ...", () => {
+  it("Has a open icon if closed", () => {
+    const { container } = render(
+      <ToggleIcon isOpen={true} OpenIcon={OpenIcon} />
+    );
+    const openIconContainer = container.firstChild.querySelector(
+      ".toogle-icon__open"
+    );
+    expect(openIconContainer).toBeTruthy();
+  });
+  it("Has a close icon if open", () => {
+    const { container } = render(
+      <ToggleIcon isOpen={false} OpenIcon={OpenIcon} CloseIcon={CloseIcon} />
+    );
+    const openIconContainer = container.firstChild.querySelector(
+      ".toogle-icon__close"
+    );
+    expect(openIconContainer).toBeTruthy();
+  });
 
-    it('It starts Closed by default', () => {
-        const {container} = render(<ToggleIcon OpenIcon={OpenIcon} CloseIcon={CloseIcon} />)
-        const openIconContainer = container.firstChild.querySelector('.toogle-icon__close')
-        expect(openIconContainer).toBeTruthy()
-    }) 
-
-})
+  it("It starts Closed by default", () => {
+    const { container } = render(
+      <ToggleIcon OpenIcon={OpenIcon} CloseIcon={CloseIcon} />
+    );
+    const openIconContainer = container.firstChild.querySelector(
+      ".toogle-icon__close"
+    );
+    expect(openIconContainer).toBeTruthy();
+  });
+});
