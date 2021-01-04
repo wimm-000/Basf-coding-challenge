754b0f83789afbe2f595d6168e77acc3eb6c7400
andres <andres@clicknaranja.com>
2021-01-04 06:44:03 +0100

    BUG: table css name caused error changed in main view

--- frontend-app/src/components/content/table/Table.jsx
+++ frontend-app/src/components/content/table/Table.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Table = () => {
   return (
-    <div className="table">
+    <div className="table-view">
       <p>
         In development, view{" "}
         <a
